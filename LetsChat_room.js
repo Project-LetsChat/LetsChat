8c77fedf6491d9d2efb1828ee40f8b163fa6fd68
Bhargav <75622003+BhargavEkbote@users.noreply.github.com>
2024-06-16 21:33:01 +0530

    Initial Commit
    Adds basic functionality.

---
+++ LetsChat_room.js
@@ -0,0 +1,55 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBQyjrjTsIQsGMGcgu-cr1HjszcHi5ZWMk",
+  authDomain: "testkwitter.firebaseapp.com",
+  databaseURL: "https://testkwitter.firebaseio.com",
+  projectId: "testkwitter",
+  storageBucket: "testkwitter.appspot.com",
+  messagingSenderId: "624653701634",
+  appId: "1:624653701634:web:2cb9a8bd873f17d92d8d1b"
+};
+
+
+
+  firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "LetsChat_page.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       Room_names = childKey;
+       console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "LetsChat_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+    window.location = "index.html";
+}
