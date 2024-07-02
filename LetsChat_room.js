5c6f2e52c5ae8a8ce673a086d769d032520b3e36
Bhargav <75622003+BhargavEkbote@users.noreply.github.com>
2024-07-02 09:20:44 +0530

    Removed backend server.
    The backend server is now only for NeoChat.

--- LetsChat_room.js
+++ LetsChat_room.js
@@ -16,13 +16,13 @@
  */
 
 firebaseConfig = {
-  apiKey: "AIzaSyDCKd9OXejZ-0PK729riphOd6Z1KgrzjCo",
-  authDomain: "letschattestbranch.firebaseapp.com",
-  databaseURL: "https://letschattestbranch-default-rtdb.firebaseio.com/",
-  projectId: "letschattestbranch",
-  storageBucket: "letschattestbranch.appspot.com",
-  messagingSenderId: "1048525961569",
-  appId: "1:1048525961569:web:83583f3ab3a726da266d20"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 }
 
 
