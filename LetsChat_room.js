40d75a2d395082cdc5582e72e060dd5aeeadf2e7
Bhargav <75622003+BhargavEkbote@users.noreply.github.com>
2024-07-22 06:30:59 +0530

    GNU Coding Standards Compliance (1)
    This is part 1 of a larger effort to improve LetsChat's compliance with the GNU Coding Standards.
    Every source code file now has clear information about LetsChat's licensing.

--- LetsChat_room.js
+++ LetsChat_room.js
@@ -1,18 +1,20 @@
-/*!
- * This file is part of LetsChat.
- * 
- * LetsChat is free software: you can redistribute it and/or modify
+/* This file is part of LetsChat - A social media platform framework
+ *
+ * Copyright (C) 2023 Bhargav
+ *
+ * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
- * 
- * YourLibrary is distributed in the hope that it will be useful,
+ *
+ * This program (LetsChat) is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
- * along with LetsChat. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program (LetsChat); if not, see <http://www.gnu.org/licenses/> or write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 firebaseConfig = {
