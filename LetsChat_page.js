432886a432758662d2f07b30847b4a21c012e7a2
Bhargav <bhargavsdeal@gmail.com>
2024-08-05 23:47:27 +0530

    05-08-2024Patch1
    This update to the source code in TESTING brings the following changes:
    - Added Links to NeoChat in the navbars placed in filter.html, LetsChat_page.html and LetsChat_room.html.
    - Re-linked the new URL of the plugin store.
    
    
    Signed-off-by: Bhargav <bhargavsdeal@gmail.com>

--- LetsChat_page.js
+++ LetsChat_page.js
@@ -1,18 +1,8 @@
-/*!
- * This file is part of LetsChat.
- * 
- * LetsChat is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 2.1 of the License, or
- * (at your option) any later version.
- * 
- * YourLibrary is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with LetsChat. If not, see <http://www.gnu.org/licenses/>.
+/* LetsChat - A social media platform framework
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
 // Firebase configuration
@@ -33,6 +23,9 @@ room_name = localStorage.getItem("room_name");
 // Function to send message
 function send() {
   msg = document.getElementById("msg").value;
+  // Sanitize message before sending
+  msg = sanitizeHTML(msg);
+
   firebase.database().ref(room_name).push({
     name: user_name,
     message: msg,
@@ -57,22 +50,23 @@ function getData() {
       childKey = childSnapshot.key;
       childData = childSnapshot.val();
       if (childKey != "purpose") {
-        firebase_message_id = childKey;
+        firebase_message_id = sanitizeHTML(childKey);
         message_data = childData;
 
         console.log(firebase_message_id);
-        console.log(message_data);
-
-        // Sanitize user inputs
-        var name = sanitizeHTML(message_data['name']);
-        var message = sanitizeHTML(message_data['message']);
-        var like = sanitizeHTML(String(message_data['like']));
+        // Sanitize message data before logging
+        var sanitized_message_data = {
+          name: sanitizeHTML(message_data['name']),
+          message: sanitizeHTML(message_data['message']),
+          like: sanitizeHTML(String(message_data['like']))
+        };
+        console.log(sanitized_message_data);
 
         // Construct HTML tags with sanitized inputs
-        var name_with_tag = "<h4>" + name + "<img class='user_tick' src='tick.png'></h4>";
-        var message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
-        var like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
-        var span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>";
+        var name_with_tag = "<h4>" + sanitized_message_data.name + "<img class='user_tick' src='tick.png'></h4>";
+        var message_with_tag = "<h4 class='message_h4'>" + sanitized_message_data.message + "</h4>";
+        var like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + sanitized_message_data.like + " onclick='updateLike(this.id)'>";
+        var span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + sanitized_message_data.like + "</span></button><hr>";
 
         // Combine the constructed tags into a row
         var row = name_with_tag + message_with_tag + like_button + span_with_tag;
