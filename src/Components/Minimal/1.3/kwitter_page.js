1dcc2a20a3aa29082031a4d182e01fb279b7750e
Bhargav <bhargavsdeal@gmail.com>
2024-08-23 10:55:31 +0530

    src update.
    Updated source code to include patches from TESTING that are to be included in the next release.
    
    Signed-off-by: Bhargav <bhargavsdeal@gmail.com>

---
+++ src/Components/Minimal/1.3/kwitter_page.js
@@ -0,0 +1,10 @@
+//YOUR FIREBASE LINKS
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+
+//End code
+      } });  }); }
+getData();
